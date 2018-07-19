bd4bbbc5a92d5d01bc80bc0edefd6f18967e6eea
Rose Sorfa <rose.otter.2015@gmail.com>
2018-07-19 14:11:06 -0500

    added pseudocode

---
+++ index.js
@@ -0,0 +1,17 @@
+/*Requirements
+In terms of user experience, your shopping list app must allow users to:
+
+enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+check and uncheck items on the list by clicking the "Check" button
+permanently remove items from the list
+Additionally:
+
+You must use a CDN-hosted version of jQuery
+Put your application code in a file called index.js and link to it in index.html
+Be sure to put both script elements at the bottom of the <body> element.
+Do not alter index.html or main.css other than adding the links to the external JavaScript inside index.html. 
+Write JavaScript code that works with the existing HTML and CSS to implement the required features.
+Hint: you may find it helpful to read up on and use the following jQuery methods: 
+.submit(), preventDefault(), toggleClass(), and closest().*/
+$('h1').fadeOut(); 
+
