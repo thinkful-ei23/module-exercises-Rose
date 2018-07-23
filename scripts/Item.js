3034206bc2722e5e2572cd656443308e560f375c
Rose Sorfa <rose.otter.2015@gmail.com>
2018-07-23 11:37:58 -0500

    added item factory and validator

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,5 +1,19 @@
 'use strict';
-
+/* global cuid, $ */
 const Item = (function () {
-  return {};
+  const validateName = (function (name) {
+    if(!name) {throw TypeError('Name does not exist');}
+  });
+  const create = (function (name){
+    return {
+      id : cuid(),
+      name, 
+      checked : false, 
+    };
+  });
+
+  return {
+    validateName,
+    create,
+  };
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,20 @@
 'use strict';
-/* global shoppingList, cuid, $ */
+/* global shoppingList, cuid, $, Item, store */
 
 // eslint-disable-next-line no-unused-vars
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
+  shoppingList.render();
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
   shoppingList.render();
 });
 
