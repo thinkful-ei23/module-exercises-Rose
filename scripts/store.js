9aad0b8ebf237616b7bea7c83ad3fd22289389f5
David Folks <aldwyn.wot@gmail.com>
2018-07-23 13:43:21 -0600

    Added CRUD methods to store

--- scripts/store.js
+++ scripts/store.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global cuid */
+/* global cuid, Item */
 const store = (function() {
   
   const items = [
@@ -7,10 +7,43 @@ const store = (function() {
   ];
   const hideCheckedItems = false;
   const searchTerm = '';
+  const findById = (function(id) {
+    return items.find(id);
+  });
+  const addItem = (function(name) {
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
+  const findAndToggleChecked = (function(id) {
+    this.findById(id).checked = !this.findById(id).checked;
+  });
+  const findAndUpdateName = (function(id, newName){
+    try {
+      Item.validateName(newName);
+      this.findById(id).name = newName;    
+    } catch(error) {
+      console.log('Cannot update name: ' + error.message);
+    }
+  });
+  const findAndDelete = (function(id){
+    let val = this.items.findIndex(function(element){
+      return element.id === id;
+    });
+    this.items.splice(val, 1);
+  });
 
   return {
     items,
     hideCheckedItems,
     searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete,
   };
 }());
